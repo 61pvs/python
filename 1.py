8c2ec4677e2762cb033ec8e4598223b3bc68a4d3
61pvs <61pvs@mail.ru>
2023-02-26 02:47:34 +0000

    1

--- 1.py
+++ 1.py
@@ -1,16 +1,14 @@
-#!/bin/python3
+def sort_array(source_array):
+    # Return a sorted array.
+    m=[]
+    for i in source_array:
+        if i%2!=0:
+            m.append(i)
+    m.sort()
+    for i in range(len(source_array)):
+        if source_array[i]%2!=0:
+            source_array[i] = m[0]
+            del m[0]
+    return source_array
 
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
-    S = input()
-    try:
-        print(int(S))
-    except ValueError:
-        print('Bad String')
+print(sort_array([5, 8, 6, 3, 4]))
